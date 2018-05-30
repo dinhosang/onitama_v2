33c4a98ed9fb47a1f1b42fc44638d99063798484
douglas sangster <douglas@an-email.home>
2018-05-31 00:48:43 +0100

    created nonConcurrentPieceFactory test and class, will check timing to test against a concurrent version

--- src/main/factories/NonConcurrentPieceFactory.java
+++ src/main/factories/NonConcurrentPieceFactory.java
@@ -1,4 +1,95 @@
 package factories;
 
+import constants.GameMode;
+import constants.PieceFaction;
+import constants.PieceType;
+import models.pieces.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 public class NonConcurrentPieceFactory {
+
+    private GameMode mode;
+    private HashMap<PieceFaction,ArrayList<Piece>> piecesContainer;
+
+    public NonConcurrentPieceFactory(){}
+
+    public void setFactory(GameMode mode, HashMap<PieceFaction,ArrayList<Piece>> piecesContainer) {
+        this.mode           = mode;
+        this.piecesContainer = piecesContainer;
+    }
+
+    public void createPieces() {
+
+        if(this.mode == GameMode.STANDARD){
+            setupStandardModePieceCreation();
+        } else {
+            //            could try throwing a homemade exception for incorrect game mode?
+        }
+    }
+
+    private void setupStandardModePieceCreation() {
+        Set<PieceFaction> keys = this.piecesContainer.keySet();
+
+        for(PieceFaction faction : keys){
+            HashMap<PieceType, Integer> piecesRequired = new HashMap<>();
+            piecesRequired.put(PieceType.STUDENT, 4);
+            piecesRequired.put(PieceType.SENSEI, 1);
+
+            beingCreateProcess(faction, piecesRequired);
+        }
+    }
+
+    private void beingCreateProcess(PieceFaction faction, HashMap<PieceType, Integer> piecesRequired) {
+        Set<PieceType> keys = piecesRequired.keySet();
+
+        for(PieceType type : keys) {
+            int numberRequired = piecesRequired.get(type);
+//            Could have a try statement in case the number is a great deal larger, in case of a memory error etc.
+//            Currently only standard mode so not required.
+            for(int numberMade = 0; numberMade < numberRequired; numberMade++){
+                createSinglePiece(faction, type);
+            }
+        }
+    }
+
+    private void createSinglePiece(PieceFaction faction, PieceType type) {
+
+        Piece piece;
+
+        if(faction == PieceFaction.RED){
+            switch(type){
+                case STUDENT:
+                    piece = new RedStudent();
+                    this.piecesContainer.get(faction).add(piece);
+                    break;
+                case SENSEI:
+                    piece = new RedSensei();
+                    this.piecesContainer.get(faction).add(piece);
+                    break;
+                default:
+                    System.out.println("Invalid type");
+                    break;
+            }
+        } else if (faction == PieceFaction.BLUE) {
+            switch (type) {
+                case STUDENT:
+                    piece = new BlueStudent();
+                    this.piecesContainer.get(faction).add(piece);
+                    break;
+                case SENSEI:
+                    piece = new BlueSensei();
+                    this.piecesContainer.get(faction).add(piece);
+                    break;
+                default:
+                    System.out.println("Invalid type");
+                    break;
+            }
+        }
+    }
+
+
+
 }
--- src/test/factoriesTest/NonConcurrentPieceFactoryTest.java
+++ src/test/factoriesTest/NonConcurrentPieceFactoryTest.java
@@ -1,12 +1,16 @@
 package factoriesTest;
 
+import constants.GameMode;
 import constants.PieceFaction;
 import factories.NonConcurrentPieceFactory;
 import models.pieces.Piece;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 public class NonConcurrentPieceFactoryTest {
 
@@ -15,7 +19,34 @@ public class NonConcurrentPieceFactoryTest {
 
     @Before
     public void before() {
+        piecesContainer = new HashMap<>();
+        piecesContainer.put(PieceFaction.BLUE, new ArrayList<>());
+        piecesContainer.put(PieceFaction.RED, new ArrayList<>());
 
+        factoryNC = new NonConcurrentPieceFactory();
+    }
+
+    @Test
+    public void piecesContainerStartsEmpty(){
+        assertEquals(0, piecesContainer.get(PieceFaction.BLUE).size());
+    }
+
+    @Test
+    public void canFillPiecesContainerWithBluePieces(){
+
+        factoryNC.setFactory(GameMode.STANDARD, piecesContainer);
+        factoryNC.createPieces();
+
+        assertEquals(5, piecesContainer.get(PieceFaction.BLUE).size());
+    }
+
+    @Test
+    public void canFillPiecesContainerWithRedPieces(){
+
+        factoryNC.setFactory(GameMode.STANDARD, piecesContainer);
+        factoryNC.createPieces();
+
+        assertEquals(5, piecesContainer.get(PieceFaction.RED).size());
     }
 
 }
