2ffeac8d3511645d46429c2c1da99cad20442366
douglas sangster <douglas@an-email.home>
2018-05-30 21:58:45 +0100

    starting testing a non-concurrent version of the piece factory, and added .gitignore

---
+++ src/test/factoriesTest/NonConcurrentPieceFactoryTest.java
@@ -0,0 +1,21 @@
+package factoriesTest;
+
+import constants.PieceFaction;
+import factories.NonConcurrentPieceFactory;
+import models.pieces.Piece;
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NonConcurrentPieceFactoryTest {
+
+    private NonConcurrentPieceFactory factoryNC;
+    private HashMap<PieceFaction, ArrayList<Piece>> piecesContainer;
+
+    @Before
+    public void before() {
+
+    }
+
+}
