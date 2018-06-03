0e644a005e8b2a5090b740209e47c2cf15826df5
douglas sangster <douglas@an-email.home>
2018-06-03 01:20:40 +0100

    created concurrentStaticCardFactory, renamed to ConcurrentByEachCardSCF.. to highlight what it is multithreading. Will make a version which creates the thread before the entire set of cards with a thread creating the entire deck, rather than creating an individual card. In terms of times, it takes roughly the same as the C piece factory, maybe slightly faster on average, even though the piece NC factory looks a good percentage faster than the card NC factory. Most of time taken for concurrent must be the thread creation overhead to account for this. I guess.

---
+++ src/main/helpers/factories/ConcurrentByEachCardStaticCardFactory.java
@@ -0,0 +1,142 @@
+package helpers.factories;
+
+import constants.GameMode;
+import helpers.IContain;
+import helpers.ICreate;
+import models.cards.Card;
+import models.cards.individualCards.*;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentByEachCardStaticCardFactory implements ICreate {
+
+    public static void createItems(GameMode mode, IContain deck) {
+
+        if(mode.equals(GameMode.STANDARD)){
+            setupStandardModeCardCreation(deck);
+        } else if (mode.equals(GameMode.TESTSTANDARD)){
+            long startTime    = System.currentTimeMillis();
+
+            for(int numberOfSetsMade = 0; numberOfSetsMade < 1000; numberOfSetsMade++){
+                setupStandardModeCardCreation(deck);
+            }
+
+            long endTime      = System.currentTimeMillis();
+            System.out.println(endTime - startTime + "ms");
+        } else {
+            // throw exception here?
+        }
+    }
+
+    private static void setupStandardModeCardCreation(IContain deck) {
+        ArrayList<Integer> randomNumbersListToCreateCardsBy = new ArrayList<>();
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        ReentrantLock lock = new ReentrantLock();
+
+//        ThreadLocalRandom.current().ints(0,19).distinct().limit(5).forEach(
+//                (int number) -> randomNumbersListToCreateCardsBy.add(number));
+
+/*      above same as below - above is lambda version, below is method reference version
+        below calls the ThreadLocalRandom which is a random number generator tied to this particular Thread
+        the .current() returns an instance of ThreadLocalRandom which was just a static call before
+        .ints(inclusiveStartPoint, exclusiveEndPoint) returns random numbers in said range of points
+        .distinct() returns unique values, .limit(int) returns that number of values
+        the .forEach determines what to do with each value */
+
+        ThreadLocalRandom.current().ints(0,19).distinct().limit(6).forEach(
+                randomNumbersListToCreateCardsBy::add);
+
+        for(Integer number : randomNumbersListToCreateCardsBy){
+
+            class concurrentSingleCardCreation implements Runnable {
+
+                private IContain    deck;
+                private Integer     number;
+
+                private concurrentSingleCardCreation(IContain deck, Integer number, ReentrantLock lock) {
+                    this.deck   = deck;
+                    this.number = number;
+                }
+
+                public void run() {
+
+                    lock.lock();
+
+                    Card card;
+
+                    switch(number){
+                        case 1:
+                            card = new Boar();
+                            break;
+                        case 2:
+                            card = new Cobra();
+                            break;
+                        case 3:
+                            card = new Crab();
+                            break;
+                        case 4:
+                            card = new Crane();
+                            break;
+                        case 5:
+                            card = new Dragon();
+                            break;
+                        case 6:
+                            card = new Eel();
+                            break;
+                        case 7:
+                            card = new Elephant();
+                            break;
+                        case 8:
+                            card = new Frog();
+                            break;
+                        case 9:
+                            card = new Goose();
+                            break;
+                        case 10:
+                            card = new Horse();
+                            break;
+                        case 11:
+                            card = new Mantis();
+                            break;
+                        case 12:
+                            card = new Monkey();
+                            break;
+                        case 13:
+                            card = new Ox();
+                            break;
+                        case 14:
+                            card = new Rabbit();
+                            break;
+                        case 15:
+                            card = new Rooster();
+                            break;
+                        default:
+                            card = new Tiger();
+                            break;
+                    }
+
+                    deck.addItem(card);
+
+                    lock.unlock();
+
+                }
+            }
+
+            concurrentSingleCardCreation cardCreator = new concurrentSingleCardCreation(deck, number, lock);
+            threadPool.execute(cardCreator);
+        }
+
+        threadPool.shutdown();
+        try {
+            threadPool.awaitTermination(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
